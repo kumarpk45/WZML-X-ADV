03f58d26dbfb24d01440c2b004e0e29bf5cf0bf2
kumarpk45 <133167314+kumarpk45@users.noreply.github.com>
2024-10-23 12:01:00 -0700

    Update fs_utils.py

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -3,9 +3,8 @@ import os
 from os import walk, path as ospath
 from aiofiles.os import remove as aioremove, path as aiopath, listdir, rmdir, makedirs
 from aioshutil import rmtree as aiormtree
-from asyncio import create_subprocess_exec
-from asyncio.subprocess import PIPE
 from shutil import rmtree, disk_usage
+from asyncio.subprocess import create_subprocess_exec, PIPE
 from magic import Magic
 from re import split as re_split, I, search as re_search
 from subprocess import run as srun
